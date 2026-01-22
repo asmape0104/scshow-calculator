13342ba71a45180f08a9b3a5b8715804c05d180a
asmape0104 <shw_star0104@yahoo.co.jp>
2026-01-23 03:05:29 +0900

    ビルドIDとタイムスタンプを更新し、HTMLファイル内のリソースパスを修正しました。また、新しいJavaScriptファイルを追加し、不要なファイルを削除しました。

--- _nuxt/DhKswkMD.js
+++ _nuxt/DhKswkMD.js
@@ -1,4 +1,4 @@
-import{f as e,c as n,a as o,t,m as i,o as s}from"./D3kprjWZ.js";const r=`THE FOLLOWING SETS FORTH ATTRIBUTION NOTICES FOR THIRD PARTY SOFTWARE THAT MAY BE CONTAINED IN PORTIONS OF THE NUXT APP PRODUCT.
+import{f as e,c as n,a as o,t,m as i,o as s}from"./DDukXa5h.js";const r=`THE FOLLOWING SETS FORTH ATTRIBUTION NOTICES FOR THIRD PARTY SOFTWARE THAT MAY BE CONTAINED IN PORTIONS OF THE NUXT APP PRODUCT.
 
 -----
 
@@ -336,6 +336,32 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+
+-----
+
+The following software may be included in this product: @bootstrap-vue-next/nuxt, bootstrap-vue-next. A copy of the source code may be downloaded from git+https://github.com/bootstrap-vue-next/bootstrap-vue-next.git (@bootstrap-vue-next/nuxt), git+https://github.com/bootstrap-vue-next/bootstrap-vue-next.git (bootstrap-vue-next). This software contains the following license and notice below:
+
+The MIT License (MIT)
+
+Copyright (c) BootstrapVueNext
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 -----
 
