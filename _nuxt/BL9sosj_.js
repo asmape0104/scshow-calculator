4f316cec0a5f170df086c150678409c34bb91f2d
asmape0104 <shw_star0104@yahoo.co.jp>
2026-01-23 12:04:42 +0900

    ビルドIDとタイムスタンプを更新し、HTMLファイル内のリソースパスを修正しました。また、不要なJavaScriptファイルを削除し、新しいビルドメタデータを追加しました。

---
+++ _nuxt/BL9sosj_.js
@@ -0,0 +1,1 @@
+import{f as o,a7 as e,a8 as n}from"./IZj0r15j.js";const _=o({__name:"auto-formation-parallel",async setup(r){let a,t;return[a,t]=e(()=>n("/auto-formation")),await a,t(),()=>{}}});export{_ as default};
